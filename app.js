2730ca47093f0bd2933b694712e4ce4e16344ed9
Bastian <maxiqboy@gmail.com>
2019-09-25 23:45:51 +0700

    URL parameters

--- app.js
+++ app.js
@@ -26,6 +26,21 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  
+  if (id < tours.length)
+  res.status(200).json({
+    status: 'success',
+    data: { tour: tours[id] }
+  });
+
+  res.status(404).json({
+    status: 'fail',
+    message: 'Invalid ID'
+  })
+});
+
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
 
