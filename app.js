87f70918908dcf9ed04508fb50e59759a28fb64d
Bastian <maxiqboy@gmail.com>
2019-09-25 21:12:50 +0700

    Starting Our API Handling GET Requests

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+const port = '6969';
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf8')
+);
 
 app.get('/', (res, rep) => {
   rep.status(200).json({
@@ -9,7 +15,13 @@ app.get('/', (res, rep) => {
   });
 });
 
-const port = '6969';
+app.get('/api/v1/tours', (res, rep) => {
+  rep.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: { tours },
+  });
+});
 
 app.listen(port, () => {
   console.log(`The server is running in port ${port} !`);
