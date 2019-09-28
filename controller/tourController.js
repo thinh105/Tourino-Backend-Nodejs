c3be98868de59e453d3e24bc4fc0ab3791015b5d
Bastian <maxiqboy@gmail.com>
2019-09-28 21:43:31 +0700

    chaining multiple middleware function

--- controller/tourController.js
+++ controller/tourController.js
@@ -52,6 +52,16 @@ exports.deleteTour = (req, res) => {
   });
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing Name or Price'
+    });
+
+  next();
+};
+
 exports.createTour = (req, res) => {
   //console.log(req.body);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,7 @@ router.param('id', tourController.checkId);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
