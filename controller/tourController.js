9ef7799ac7692d2990a6c8c416ccfd008fae211a
Bastian <maxiqboy@gmail.com>
2019-11-07 00:21:14 +0700

    Adding 404 Not Found Errors

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // ROUTE HANDLERS
 
@@ -127,6 +128,8 @@ exports.aliasTopFiveTours = (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) return next(new AppError('No tour found with that ID!!!', 404));
+
   res.status(200).json({
     status: 'success',
     data: { tour }
@@ -139,6 +142,8 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   });
 
+  if (!tour) return next(new AppError('No tour found with that ID!!!', 404));
+
   res.status(200).json({
     status: 'success',
     dataUpdate: { tour }
@@ -146,8 +151,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) return next(new AppError('No tour found with that ID!!!', 404));
+
   // in RESTful API, common practice is not send anything back to client when deleted
+
   res.status(204).json({
     status: 'success',
     data: null
